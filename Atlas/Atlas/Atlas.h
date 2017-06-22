0f388c184ac843ea06c45a09da7843e6b828a718
Engel, Stefan <Stefan.Engel@dm.de>
2017-06-22 10:05:54 +0200

    Initial commit containing the project, .gitignore and Cartfile.private

---
+++ Atlas/Atlas/Atlas.h
@@ -0,0 +1,19 @@
+//
+//  Atlas.h
+//  Atlas
+//
+//  Created by Engel, Stefan on 22.06.17.
+//  Copyright © 2017 dm-drogerie markt GmbH + Co. KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Atlas.
+FOUNDATION_EXPORT double AtlasVersionNumber;
+
+//! Project version string for Atlas.
+FOUNDATION_EXPORT const unsigned char AtlasVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Atlas/PublicHeader.h>
+
+
