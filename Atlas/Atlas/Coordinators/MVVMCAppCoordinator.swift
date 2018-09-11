bcf1941178574b15cd6e127f9b726ca253c8f829
Ehlen, David <david.ehlen@dm.de>
2018-09-11 16:30:29 +0200

    customize navigation and tabbar controller

--- Atlas/Atlas/Coordinators/MVVMCAppCoordinator.swift
+++ Atlas/Atlas/Coordinators/MVVMCAppCoordinator.swift
@@ -33,7 +33,7 @@ public class MVVMCAppCoordinator: NSObject {
         var rootViewControllers: [UINavigationController] = []
 
         for factory in factories {
-            let navigationController = setupTabBarItem(image: factory.unselectedTabBarIconImage, selectedImage: factory.selectedTabBarIconImage)
+            let navigationController = setupTabBarItem(image: factory.unselectedTabBarIconImage, selectedImage: factory.selectedTabBarIconImage, prefersLargeTitles: factory.prefersLargeTitles)
             let coordinator = MVVMCCoordinator(model: model, navigationController: navigationController, factory: factory)
             let module = MVVMCModule(factory: factory, navigationController: navigationController, coordinator: coordinator)
 
@@ -42,13 +42,19 @@ public class MVVMCAppCoordinator: NSObject {
         }
 
         tabBar.setViewControllers(rootViewControllers, animated: false)
+        tabBar.tabBar.isTranslucent = false
     }
 
-    func setupTabBarItem(image: UIImage?, selectedImage: UIImage?) -> UINavigationController {
+    func setupTabBarItem(image: UIImage?, selectedImage: UIImage?, prefersLargeTitles: Bool = true) -> UINavigationController {
         let navController = UINavigationController()
+        navController.navigationBar.isTranslucent = false
+        if #available(iOS 11.0, *) {
+            navController.navigationBar.prefersLargeTitles = prefersLargeTitles
+        }
         navController.tabBarItem = UITabBarItem(title: nil, image: image, selectedImage: selectedImage)
         navController.tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
-        
+
+        navController.view.backgroundColor = UIColor.white
         return navController
     }
 }
--- Atlas/Atlas/Protocols/MVVMCTabBarFactoryProtocol.swift
+++ Atlas/Atlas/Protocols/MVVMCTabBarFactoryProtocol.swift
@@ -3,4 +3,5 @@ import UIKit
 public protocol MVVMCTabBarFactoryProtocol: MVVMCFactoryProtocol {
     var selectedTabBarIconImage: UIImage? { get }
     var unselectedTabBarIconImage: UIImage? { get }
+    var prefersLargeTitles: Bool { get }
 }
