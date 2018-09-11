db220f2df079542d949f3bb5bb343a92ffcffcb4
Ehlen, David <david.ehlen@dm.de>
2018-09-11 16:29:08 +0200

    make transition type equatable

--- Atlas/Atlas/Configuration/MVVMCTransitionType.swift
+++ Atlas/Atlas/Configuration/MVVMCTransitionType.swift
@@ -2,3 +2,15 @@ public enum MVVMCTransitionType {
     case modal(animated: Bool)
     case push(animated: Bool)
 }
+
+// MARK: - Equatable
+extension MVVMCTransitionType: Equatable {
+    public static func == (lhs: MVVMCTransitionType, rhs: MVVMCTransitionType) -> Bool {
+        switch (lhs, rhs) {
+        case (.modal, .modal): return true
+        case let (.push(leftAnimated), .push(rightAnimated)): return leftAnimated == rightAnimated
+        default: return false
+        }
+    }
+}
+
