4fef762f9ef47362382435e1f757107fa69471ce
Engel, Stefan <Stefan.Engel@dm.de>
2018-09-17 08:54:24 +0200

    feature: Update models to conform to new protocol

--- Atlas/AtlasDemo/EmptyModel.swift
+++ Atlas/AtlasDemo/EmptyModel.swift
@@ -1,5 +1,9 @@
 import Atlas
 
 class EmptyModel: MVVMCModelProtocol {
-    
+    func register(observer: MVVMCModelObserver) {
+    }
+
+    func deregister(observer: MVVMCModelObserver) {
+    }
 }
--- Atlas/AtlasTests/Mocks/ModelMock.swift
+++ Atlas/AtlasTests/Mocks/ModelMock.swift
@@ -1,4 +1,9 @@
 import Atlas
 
 class ModelMock: MVVMCModelProtocol {
+    func register(observer: MVVMCModelObserver) {
+    }
+
+    func deregister(observer: MVVMCModelObserver) {
+    }
 }
