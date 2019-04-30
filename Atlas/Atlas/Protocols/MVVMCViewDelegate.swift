74add34739df2c9f8932e22a89b1153d601c1d3a
Stefan Engel <stefan.engel@dm.de>
2019-04-30 18:27:55 +0200

    chore: Deprecate reload

--- Atlas/Atlas/Protocols/MVVMCViewDelegate.swift
+++ Atlas/Atlas/Protocols/MVVMCViewDelegate.swift
@@ -2,5 +2,7 @@ import UIKit
 
 public protocol MVVMCViewDelegate: class {
     func request(navigation request: MVVMCNavigationRequest, withData data: [String: Any]?)
+    
+    @available(*, deprecated, message: "Use update and handle the view updates in the factory")
     func reload()
 }
