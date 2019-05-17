6a326d7dfff6b1bb985c9f51432648f4f924e8dc
Ehlen, David <david.ehlen@dm.de>
2019-05-17 09:09:53 +0200

    added hook for customization after a module was loaded

--- Atlas/Atlas/Coordinators/MVVMCAppCoordinator.swift
+++ Atlas/Atlas/Coordinators/MVVMCAppCoordinator.swift
@@ -39,6 +39,8 @@ public class MVVMCAppCoordinator: NSObject {
 
             rootViewControllers.append(navigationController)
             modules.append(module)
+
+            factory.didSetupModule?(navigationController, tabBar, coordinator)
         }
 
         tabBar.setViewControllers(rootViewControllers, animated: false)
