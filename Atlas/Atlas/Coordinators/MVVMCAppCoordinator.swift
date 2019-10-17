887ed22b94a465fb7dc8e2e29af4657e18ee0862
Christoph Wendt <christoph.wendt@dm.de>
2019-10-17 14:32:13 +0200

    feature: support tab bar item titles

--- Atlas/Atlas/Coordinators/MVVMCAppCoordinator.swift
+++ Atlas/Atlas/Coordinators/MVVMCAppCoordinator.swift
@@ -33,7 +33,7 @@ public class MVVMCAppCoordinator: NSObject {
         var rootViewControllers: [UINavigationController] = []
 
         for factory in factories {
-            let navigationController = setupTabBarItem(image: factory.unselectedTabBarIconImage, selectedImage: factory.selectedTabBarIconImage, prefersLargeTitles: factory.prefersLargeTitles)
+            let navigationController = setupTabBarItem(title: factory.tabBarItemTitle, image: factory.unselectedTabBarIconImage, selectedImage: factory.selectedTabBarIconImage, prefersLargeTitles: factory.prefersLargeTitles)
             let coordinator = MVVMCCoordinator(model: model, navigationController: navigationController, factory: factory)
             let module = MVVMCModule(factory: factory, navigationController: navigationController, coordinator: coordinator)
 
@@ -47,15 +47,14 @@ public class MVVMCAppCoordinator: NSObject {
         tabBar.tabBar.isTranslucent = false
     }
 
-    func setupTabBarItem(image: UIImage?, selectedImage: UIImage?, prefersLargeTitles: Bool = true) -> UINavigationController {
+    func setupTabBarItem(title: String?, image: UIImage?, selectedImage: UIImage?, prefersLargeTitles: Bool = true) -> UINavigationController {
         let navController = UINavigationController()
         navController.navigationBar.isTranslucent = false
         if #available(iOS 11.0, *) {
             navController.navigationBar.prefersLargeTitles = prefersLargeTitles
         }
-        navController.tabBarItem = UITabBarItem(title: nil, image: image, selectedImage: selectedImage)
-        navController.tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
-
+        navController.tabBarItem = UITabBarItem(title: title, image: image, selectedImage: selectedImage)
+        
         navController.view.backgroundColor = UIColor.white
         return navController
     }
